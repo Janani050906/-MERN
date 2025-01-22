2ceab1450f408929627b56fc00a3b0f9418b67c5
Janani0509 <jananiaarasu@gmail.com>
2025-01-22 10:24:47 +0530

    index.html1

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html><head></head>
+    <title>JS</title>
+    <script src="script.js"></script>
+    <h1>Javascript</h1>
+    </head>
+</html>
+<h1 id="ex">Hello ece</h1>
+<button onclick="SecondButtonClicked()">Click</button>
+<script>console.log("Hello world!");
+    function buttonClickd(){
+        document.getElementById('ex')
+    }
+</script>
+</body>
+</html>
